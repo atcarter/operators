3d72e3a333d9d5fda0650e058fe35e59b722f99a
Antonio <atcarter005@gmail.com>
2020-12-09 20:48:38 -0600

    commit tree 4f5026ddb7d5367d094ce2f788a62112ce22ef21

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,11 +1,18 @@
 def unsafe?(speed)
-
+    bool = false
+    if speed < 40 || speed > 60
+        bool = true
+        bool
+    else
+        bool
+    end
+    
 end
 
 
 
 def not_safe?(speed)
-	
+    speed > 60 || speed < 40 ? true : false
 end
 	
 
